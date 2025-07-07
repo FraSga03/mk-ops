7a8956d3f34f34347b3fabc429725682d1747fa0
FRA <frasga@gmail.com>
2025-07-07 14:22:58 +0200

    ref(sound): Add door sound, fix shaders and translation

--- scripts/immovable_objects/immovable_exit_door.gd
+++ scripts/immovable_objects/immovable_exit_door.gd
@@ -1,6 +1,9 @@
 extends StaticBody3D
 
+@onready var door_sound_player: AudioStreamPlayer3D = $DoorSoundPlayer
+
 signal on_door_entered;
 
 func _on_area_3d_body_entered(_body: Node3D) -> void:
+	door_sound_player.play();
 	on_door_entered.emit();
